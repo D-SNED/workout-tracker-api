a37d4195e917c722e1fc724d82a39fb3f1af13f7
DSnediker <dsnediker7@gmail.com>
2025-07-27 10:32:59 -0700

    created workout model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Workout (models.Model):
+
+    WORKOUT_TYPE = [
+    ("push", "Push"),
+    ("pull", "Pull"),
+    ("legs", "Legs"),
+    ("run", "Run"),
+    ("mobility", "Mobility"),
+    ]
+
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    workout_type = models.CharField(max_length=20, choices=WORKOUT_TYPE)
+    date = models.DateField(auto_now_add=True)
+    notes = models.TextField(blank=True)
