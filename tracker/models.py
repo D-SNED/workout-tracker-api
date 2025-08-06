c2ff5af65b1d9b45e5cff8b03bd462fa72ae909f
DSnediker <dsnediker7@gmail.com>
2025-08-05 17:42:40 -0700

    added ExerciseLog and SetLog classes

--- tracker/models.py
+++ tracker/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Workout (models.Model):
 
@@ -10,7 +13,18 @@ class Workout (models.Model):
     ("mobility", "Mobility"),
     ]
 
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     workout_type = models.CharField(max_length=20, choices=WORKOUT_TYPES)
     date = models.DateField(auto_now_add=True)
     notes = models.TextField(blank=True)
+
+class ExerciseLog (models.Model):
+
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name="exercises")
+    exercise_name = models.CharField(max_length=50)
+
+
+class SetLog (models.Model):
+
+    exercise_log_id = models.ForeignKey(ExerciseLog, on_delete=models.CASCADE)
+    set_number = models.IntegerField()
