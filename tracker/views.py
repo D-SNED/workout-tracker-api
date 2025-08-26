b5a9eb6ceb895e3bc07bd574ebcd6133cbeef1f5
DSnediker <dsnediker7@gmail.com>
2025-08-26 13:18:48 -0700

    simplified viewsets and changed permissions in settings.py to allow any during building/testing

--- tracker/views.py
+++ tracker/views.py
@@ -2,16 +2,9 @@ from rest_framework import viewsets
 from .models import Workout, ExerciseLog, SetLog
 from .serializers import WorkoutSerializer, ExerciseLogSerializer, SetLogSerializer
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 
 
 class WorkoutViewSet(viewsets.ModelViewSet):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
-    permission_classes = [AllowAny]
 
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
