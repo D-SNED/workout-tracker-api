1ed660195808f69e17c80fca1b9df2c8fa4c46e1
DSnediker <dsnediker7@gmail.com>
2025-10-10 13:19:00 -0700

    deleted routes.py and added into urls.py in workout tracker app rather than tracker app. Felt more readable this way.

--- tracker/routes.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import WorkoutViewSet, ExerciseViewSet, SetViewSet
-
-router = DefaultRouter()
-router.register(r'workouts', WorkoutViewSet, basename='workout')
-router.register(r'exercises', ExerciseViewSet, basename='exercise')
-router.register(r'sets', SetViewSet, basename='set')
-
-urlpatterns = router.urls
