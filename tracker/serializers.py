2565bbbbe49ce64b2bb9a9b04869a54277a11b8b
DSnediker <dsnediker7@gmail.com>
2025-10-10 13:10:19 -0700

    removed nesting from serializers. Will add back in at a later date.

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -8,28 +8,13 @@ class SetSerializer(serializers.ModelSerializer):
 
 class ExerciseSerializer(serializers.ModelSerializer):
 
-    sets = SetSerializer(many=True, read_only=True)
-
     class Meta:
         model = Exercise
-        fields = ["id", "name", "workout", "sets"]
-
-    def get_fields(self):
-        # print("inside get fields")
-        fields = super().get_fields()
-        request = self.context.get("request")
-        # print(self.context)
-        if request and request.user.is_authenticated:
-            fields["workout"].queryset = Workout.objects.filter(user=request.user)
-        return fields
-
+        fields = ["id", "workout", "name"]
 
 
 class WorkoutSerializer(serializers.ModelSerializer):
 
-    exercises = ExerciseSerializer(many=True, read_only=True)
-    user = serializers.StringRelatedField(read_only=True)
-
     class Meta:
         model = Workout
-        fields = ["id", "user", "workout_type", "date", "notes", "exercises"]
+        fields = ["id", "user", "workout_type", "date", "notes"]
