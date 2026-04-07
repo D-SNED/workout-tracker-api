bb5b28648a9d76bf00cbc9a6f504c3f948afea0d
DSnediker <dsnediker7@gmail.com>
2026-04-06 20:21:05 -0700

    added routes in app

--- workout-tracker-frontend/src/App.jsx
+++ workout-tracker-frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Summary from "./pages/Summary";
+import WorkoutPage from "./pages/WorkoutPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>Workout Tracker</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/workout" element={<WorkoutPage />} />
+        <Route path="/summary" element={<Summary />} />
+      </Routes>
     </BrowserRouter>
   );
 }
